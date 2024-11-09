f9ee95e2f2ad884aa18e6f729bdef78d124ef7be
Zoey <124947648+XingChenwa@users.noreply.github.com>
2024-11-10 00:14:43 +0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ games { 'gta5' }
 author 'xinqing'
 description 'FiveM'
 version '1.1.0'
-repository 'https://github.com/XingChenwa/xinqing_ban/'
+repository 'https://github.com/XingChenwa/xinqing_ban'
 
 server_scripts {
   'server.lua'
